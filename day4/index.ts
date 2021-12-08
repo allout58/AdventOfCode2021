99488c9fbd0f47c152256b5baea0f914ee7b6671
James Hollowell <jamesthollowell@gmail.com>
2021-12-07 22:32:17 -0500

    Day 4 part1 complete

--- day4/index.ts
+++ day4/index.ts
@@ -7,7 +7,7 @@ export default function (logger: Consola, testMode: boolean) {
 
 class Board {
   boardSize = 5;
-  constructor(private board: number[][]) { }
+  constructor(private board: number[][], private logger: Consola) { }
 
   private checkX(row: number): boolean {
     return this.board[row].every(x => x === -1);
@@ -21,7 +21,7 @@ class Board {
     let down = true, up = true;
     for (let i = 0; i < this.boardSize; i++) {
       down = down && this.board[i][i] === -1;
-      up = up && this.board[this.boardSize - i][i] === -1;
+      up = up && this.board[this.boardSize - i - 2][i] === -1;
     }
 
     return up || down;
@@ -40,9 +40,13 @@ class Board {
     return false;
   }
 
+  public logBoard() {
+    this.logger.debug('\n' + this.board.map(row => row.map(x => x.toString().padStart(2, ' ')).join(' ')).join('\n'));
+  }
+
   public sumNotMarked(): number {
     return this.board.reduce((sum, row) => {
-      return sum + row.filter(x => x !== -1).reduce((s2, col) => s2 + col)
+      return sum + row.filter(x => x !== -1).reduce((s2, col) => s2 + col, 0)
     }, 0)
   }
 }
@@ -53,12 +57,17 @@ class Day4 extends Day {
     const boards: Board[] = [];
     let row = 2;
     while (row < input.length) {
-      boards.push(new Board(input.slice(row, row + 5).map(x => x.split(' ').map(y => +y))))
+      boards.push(new Board(input.slice(row, row + 5).map(x => x.split(' ').filter(y => !!y).map(y => +y)), this.logger))
+      row += 6;
     }
     for (const draw of draws) {
+      this.logger.debug(`Draw: ${draw}`);
       for (const board of boards) {
+        board.logBoard();
         if (board.mark(draw)) {
-          return board.sumNotMarked() + '';
+          this.logger.debug('Found board')
+          board.logBoard()
+          return (board.sumNotMarked() * draw) + '';
         }
       }
     }
@@ -66,6 +75,28 @@ class Day4 extends Day {
   }
 
   async part2(input: string[]): Promise<string> {
-    return '';
+    const draws = input[0].split(',').map(x => +x);
+    const boards: Board[] = [];
+    let row = 2;
+    while (row < input.length) {
+      boards.push(new Board(input.slice(row, row + 5).map(x => x.split(' ').filter(y => !!y).map(y => +y)), this.logger))
+      row += 6;
+    }
+    for (const draw of draws) {
+      this.logger.debug(`Draw: ${draw}`);
+      for (let b = 0; b < boards.length; b++) {
+        const board = boards[b];
+        board.logBoard();
+        if (board.mark(draw)) {
+          boards.splice(b, 1);
+          this.logger.debug('Found board')
+          board.logBoard()
+          if (boards.length === 0) {
+            return (board.sumNotMarked() * draw) + '';
+          }
+        }
+      }
+    }
+    return 'not found';
   }
 }
